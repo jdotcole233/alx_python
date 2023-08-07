73af2f5d157e94758e1020b729b731754294f663
Jay Cole <jdotcole233@gmail.com>
2023-08-07 07:13:07 -0500

    docs

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #! /usr/bin/python3
+'''Classes demonstration
+        
+    This class is a demonstration of how 
+    classes  and private attributes are 
+    created
+'''
 
 class Square:
     '''Classes demonstration
@@ -7,6 +13,7 @@ class Square:
         classes  and private attributes are 
         created
     '''
+
     def __init__(self, size):
         '''A constructor with the single 
             private variable

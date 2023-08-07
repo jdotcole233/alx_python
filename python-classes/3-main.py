ac5be143b4b2ad1b534eeddea06a6e34f06e4232
Jay Cole <jdotcole233@gmail.com>
2023-08-07 10:05:35 -0500

    three

--- python-classes/3-main.py
+++ python-classes/3-main.py
@@ -1,29 +1,14 @@
-#! /usr/bin/python3
-"""
-    Class demonstration to show setters and getters
-"""
+#!/usr/bin/python3
+Square = __import__('3-square').Square
 
-class Square:
-    """
-        class property definitions
-    """
-    def __init__(self, size=0):
-        self.__size = size
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
 
-    def area(self):
-        return self.__size * self.__size
-    
-    @property
-    def size(self):
-        return self.__size
-    
-    @size.setter
-    def size(self, value):
-        if not type(value) == int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        
-        self.__size = value
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
 
-        
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)

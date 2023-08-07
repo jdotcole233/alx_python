72c3aea48b40fbb1fc44feb5beb4494d156126cc
Jay Cole <jdotcole233@gmail.com>
2023-08-07 06:43:34 -0500

    classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,7 +2,13 @@
 
 class Square:
     """ 
-        Square classes
+        This class is a demonstration of how 
+        classes  and private attributes are 
+        created
     """
     def __init__(self, size):
+        '''
+            A constructor with the single 
+            private variable
+        '''
         self.__size = size

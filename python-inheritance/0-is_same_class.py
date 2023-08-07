c2049162e0030c5dac4402dc80b8fd56ebf68ef9
Jay Cole <jdotcole233@gmail.com>
2023-08-07 10:30:26 -0500

    documenation

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -4,4 +4,7 @@
 """
 def is_same_class(obj, a_class):
     """Function documentation goes here"""
-    return isinstance(obj, a_class)
+    if isinstance(obj, a_class) == True:
+        return True
+    
+    return False

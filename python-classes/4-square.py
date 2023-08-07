5909ebeb57746ae5c8fea737743c942df1307f71
Jay Cole <jdotcole233@gmail.com>
2023-08-07 10:15:33 -0500

    four

---
+++ python-classes/4-square.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3
+
+"""
+    Module documentation goes here
+"""
+
+class Square:
+    """
+        Class documentation also goes here.
+    """
+
+    def __init__ (self, size=0):
+        self.__size = size
+
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not type(value) == int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+    
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        
+        for row in range(self.__size):
+            for col in range(self.__size):
+                print("#", end="")
+            print()
+

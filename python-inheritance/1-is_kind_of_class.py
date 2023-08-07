b2340395b8aea7955e392cd2b24aade26730cee1
Jay Cole <jdotcole233@gmail.com>
2023-08-07 10:56:58 -0500

    kinda

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+"""
+    Module documentation goes here
+"""
+def is_kind_of_class(obj, a_class):
+    """Function documentation goes here"""
+    return isinstance(obj, a_class)

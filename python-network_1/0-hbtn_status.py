a197ad1d60de1a42468e3f6d5490188e90d533fa
Jay Cole <jdotcole233@gmail.com>
2023-08-08 04:04:50 -0500

    docs

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,6 +4,6 @@ import requests
     Request module demonstration comments goes here
 """
 request = requests.get('https://alu-intranet.hbtn.io/status')
-print("Body response:$", end="\n")
-print(f"\t- type: {type(request.text)}$")
-print(f"\t- content: {request.text}$")
+print("Body response:", end="\n")
+print(f"\t- type: {type(request.text)}")
+print(f"\t- content: {request.text}")

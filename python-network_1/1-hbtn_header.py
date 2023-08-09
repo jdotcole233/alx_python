cbdf625074c98febb6f52547eb5e10fbdd770a1d
Jay Cole <jdotcole233@gmail.com>
2023-08-09 17:52:39 -0500

    code

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,8 +1,8 @@
-"""Request module demonstration comments goes here"""
 #!/usr/bin/python3
+# Request module demonstration comments goes here
+
 import requests
 import sys
-"""Request module demonstration comments goes here"""
 req = requests.get(sys.argv[1])
 print(req.headers['X-Request-Id'])
 

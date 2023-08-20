56eedc509a0e6422dd37479f2b37b29c4b7d01be
Jay Cole <jdotcole233@gmail.com>
2023-08-20 05:19:00 -0500

    docs

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+"""filter states with name starting from N from db"""
+#! /usr/bin/python3
+
+import MySQLdb
+import sys
+
+# Some comments that goes here
+
+host = "localhost"
+port = 3306
+user = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+search_term = sys.argv[4]
+
+
+db_connect = MySQLdb.connect(
+    host=host, user=user, passwd=password, db=database, port=port)
+
+cursor = db_connect.cursor()
+statement = "SELECT * FROM states WHERE name = {}".format(search_term)
+
+cursor.execute(statement)
+results = cursor.fetchall()
+
+for result in results:
+    print(result)

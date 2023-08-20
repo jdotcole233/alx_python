3085dd4d72d75b0516edb4aaa697422e9b7bddab
Jay Cole <jdotcole233@gmail.com>
2023-08-20 04:50:21 -0500

    docs

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+"""filter states with name starting from N from db"""
+#! /usr/bin/python3
+
+import MySQLdb
+import sys
+
+
+host = "localhost"
+port = 3306
+user = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+
+db_connect = MySQLdb.connect(
+    host=host, user=user, passwd=password, db=database, port=port)
+
+cursor = db_connect.cursor()
+statement = """
+    SELECT * FROM states WHERE name Like 'N%'
+"""
+
+cursor.execute(statement)
+results = cursor.fetchall()
+
+for result in results:
+    print(result)

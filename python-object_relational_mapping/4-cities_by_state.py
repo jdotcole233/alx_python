057988182af655490bd259af20ea4bd87c987263
Jay Cole <jdotcole233@gmail.com>
2023-08-20 06:04:02 -0500

    docs

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+# Some comments that goes here
+# Some additional comments
+# More
+import MySQLdb
+import sys
+
+host = "localhost"
+port = 3306
+user = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+db_connect = MySQLdb.connect(
+    host=host, user=user, passwd=password, db=database, port=port)
+
+cursor = db_connect.cursor()
+statement = """
+    SELECT * FROM cities
+"""
+
+cursor.execute(statement)
+results = cursor.fetchall()
+
+for result in results:
+    print(result)

57571622c16e4065c936719a58c2a93535e22f1c
Jay Cole <jdotcole233@gmail.com>
2023-08-20 06:18:50 -0500

    docs

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+# Some comments that goes here
+# Some additional comments
+# More
+import MySQLdb
+import sys
+
+host = "localhost"
+port = 3306
+user = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+search_term = sys.argv[4]
+
+db_connect = MySQLdb.connect(
+    host=host, user=user, passwd=password, db=database, port=port)
+
+cursor = db_connect.cursor()
+statement = """
+    SELECT cities.id, cities.name, states.name FROM cities
+    JOIN states on cities.state_id=states.id
+"""
+
+cursor.execute(statement)
+results = cursor.fetchall()
+
+for result in results:
+    if result[3] == search_term:
+        print(result[2], end=", ")

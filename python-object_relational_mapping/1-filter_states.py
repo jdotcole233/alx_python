dde4e7e66c1f9c233ff39f6e2755ad08fa09a0f2
Jay Cole <jdotcole233@gmail.com>
2023-08-20 05:34:00 -0500

    docs

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,15 @@
-"""filter states with name starting from N from db"""
 #! /usr/bin/python3
-
+# Some comments that goes here
+# Some additional comments
+# More
 import MySQLdb
 import sys
-
-# Some comments that goes here
 
 host = "localhost"
 port = 3306
 user = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
-
 
 db_connect = MySQLdb.connect(
     host=host, user=user, passwd=password, db=database, port=port)

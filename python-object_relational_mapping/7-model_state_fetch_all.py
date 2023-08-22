71e2b7e3477f643aaa1259e73e636a647d963a85
Jay Cole <jdotcole233@gmail.com>
2023-08-22 04:08:31 -0500

    docs

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+# Some comments goes here
+# Some more comments goes here
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                       .format(username, password, database))
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+states = session.query(State).all()
+counter = 1
+for state in states:
+    print("{}: {}".format(counter, state.name))

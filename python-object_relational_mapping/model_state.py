a866df2febc5712e5b291ab89e27df53fabbe482
Jay Cole <jdotcole233@gmail.com>
2023-08-22 03:37:53 -0500

    docs

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,6 +8,9 @@ Base = declarative_base()
         Some class comments must go here
 """
 class State(Base):
+    """
+        Some class comments must go here
+    """
     __tablename__ = "states"
     id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)

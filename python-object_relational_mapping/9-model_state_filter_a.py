5db080aa06168a5408b4cc04acbb5a567571a088
Jay Cole <jdotcole233@gmail.com>
2023-08-22 05:59:50 -0500

    docs

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+# Some comments goes here
+# Some comments goes here
+# Some comments goes here
+import sys
+from sqlalchemy import create_engine
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                       .format(username, password, database))
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+states = session.query(State).all()
+counter = 1
+for state in states:
+    if 'a' in state:
+        print("{}: {}".format(counter, state))
+        counter = counter + 1

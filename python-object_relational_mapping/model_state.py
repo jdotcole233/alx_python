f1dfb39efd95e7832f4d974afbf58fead0313fcc
Jay Cole <jdotcole233@gmail.com>
2023-08-24 05:15:01 -0500

    class docs

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,10 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
-"""
-        Some class comments must go here
-"""
-
 
 class State(Base):
 

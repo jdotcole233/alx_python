639f82f4c19db70bcc9c32fa286e66f0722a2f19
Jay Cole <jdotcole233@gmail.com>
2023-08-24 05:28:08 -0500

    doc test

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,18 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-import MySQLdb
-
-username = 'root'
-password = 'St10285515'
-database = 'test_6'
-# Create connection URL
-connector = "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, database)
-
-# Create an SQLAlchemy engine
-engine = create_engine(connector)
-
-# Establish a connection to the database
-connection = engine.connect()
 
 # Base class
 Base = declarative_base()
@@ -36,6 +22,3 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-Base.metadata.create_all(bind=engine)

d99d245181ea1a7436524d831211d23d35172d29
Jay Cole <jdotcole233@gmail.com>
2023-08-25 10:52:59 -0500

    docs

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+"""
+    python flask basic server
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """return string"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5000")

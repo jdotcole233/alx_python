21ba6760eb63f75ea42bc7c7f185788e0fa0bf96
Jay Cole <jdotcole233@gmail.com>
2023-08-25 10:48:28 -0500

    docs

---
+++ python-web_framework/0-hello_route.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+"""
+    python flask basic server
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def home():
+    """return string"""
+    return "Hello HBNB"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5000")

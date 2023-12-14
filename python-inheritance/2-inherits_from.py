3af3561655fab54a946db3524a03e77e44c2556f
Jay Cole <jdotcole233@gmail.com>
2023-12-14 06:18:49 -0600

    docs

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,6 +2,7 @@
   Module documentation goes here
 """
 def inherits_from(obj, a_class):
+   """Function documentation goes here"""
    if isinstance(obj, a_class) or issubclass(obj, a_class):
       return True
    
